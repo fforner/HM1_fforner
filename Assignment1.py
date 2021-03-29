577744fd3b59eef7f7d0db636f1a33a4dc228300
giovanniscalzotto <gscalzot@stevens.edu>
2021-03-29 11:46:29 -0400

    Add Tanget

--- Assignment1.py
+++ Assignment1.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 x = np.arange(0, 2*np.pi, 0.1)
 y = np.sin(x)
 z = np.cos(x)
+w = np.tan(x)
 
+plt.plot(x, y, x, z, x, w)
 
 plt.plot(x, y, x, z)
-#plt.xlim(0, 10)
-#plt.ylim(-1.1, 1.1)
+
 plt.xlabel('x from 0 to 2*pi', fontsize = 12)
 plt.ylabel('Sin, Cos', fontsize = 12) #ff change label
-plt.legend(['Sin', 'Cos'], loc = "upper right", frameon = False, fontsize = 10) #ff change legend
+plt.legend(['Sin', 'Cos','Tan'], loc = "upper right", frameon = False, fontsize = 10) #ff change legend
 plt.title('Assignment 1', loc='left', fontsize = 20)
 
 plt.axhline(y=0, color = 'black') #ff add graphical axis
