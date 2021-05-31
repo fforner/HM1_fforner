829489547fdaf228579d01f07ce2f61b726d84b8
nothiefhouse <fforner@stevens.edu>
2021-05-30 21:58:48 -0400

    Changes after Pycharm control

--- Assignment1.py
+++ Assignment1.py
@@ -1,23 +1,23 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 2*np.pi, 0.1)
+x = np.arange(0, 2 * np.pi, 0.1)
 y = np.sin(x)
 z = np.cos(x)
 
-
 plt.plot(x, y, x, z)
-#plt.xlim(0, 10)
-#plt.ylim(-1.1, 1.1)
-plt.xlabel('x from 0 to 2*pi', fontsize = 12)
-plt.ylabel('Sin, Cos', fontsize = 12) #ff change label
-plt.legend(['Sin', 'Cos'], loc = "upper right", frameon = False, fontsize = 10) #ff change legend
-plt.title('Assignment 1', loc='left', fontsize = 20)
 
-plt.axhline(y=0, color = 'black') #ff add graphical axis
-plt.axhline(y=0, color = 'black') #ff add graphical axis
-axes = plt.gca() #ff get the current Axes instance
-axes.set_ylim([-1.5 , 1.5 ]) #ff ste y axes limit
+# plt.xlim(0, 10)
+# plt.ylim(-1.1, 1.1)
+
+plt.xlabel('x from 0 to 2*pi', fontsize=12)
+plt.ylabel('Sin, Cos', fontsize=12)  # ff change label
+plt.legend(['Sin', 'Cos'], loc="upper right", frameon=False, fontsize=10)  # ff change legend
+plt.title('Assignment 1', loc='left', fontsize=20)
+
+plt.axhline(y=0, color='black')  # ff add graphical axis
+plt.axhline(y=0, color='black')  # ff add graphical axis
+axes = plt.gca()  # ff get the current Axes instance
+axes.set_ylim([-1.5, 1.5])  # ff ste y axes limit
 
 plt.show()
